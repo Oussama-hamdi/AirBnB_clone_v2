9961462d170f3be0daa34bffc7c6580a1c11d73f
root <ohamdi776@gmail.com>
2023-10-22 06:27:15 -0700

    Completed 9. Cities by states task

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -9,9 +9,9 @@ app = Flask(__name__)
 
 @app.route('/states_list', strict_slashes=False)
 def states_list():
-    """Display a list of states from db"""
+    """Display a list of states"""
     states = storage.all(State).values()
-    return render_template("7-states_list.html", states=states)
+    return render_template("8-cities_by_states.html", states=states)
 
 
 @app.teardown_appcontext
